65ae762b119f4e9cc919d98914b64c3ae474e257
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-03 19:11:21 -0600

    narrow down repo mapping to modules folder only

--- lmake_z.sh
+++ lmake_z.sh
@@ -2,5 +2,5 @@
 param1=$1
 echo $1
 echo $param1
-echo docker run -v "$(pwd)/:/home/ubuntu/work/" -v "$(pwd)/$param1:/home/ubuntu/work/todo.sh" -it --rm -u ubuntu bbb_amd:0.14
-sudo docker run -v "$(pwd)/:/home/ubuntu/work/" -v "$(pwd)/$param1:/home/ubuntu/work/todo.sh" -it --rm -u ubuntu bbb_amd:0.14
+echo docker run -v "$(pwd)/modules/:/home/ubuntu/work/modules/" -v "$(pwd)/$param1:/home/ubuntu/work/todo.sh" -it --rm -u ubuntu bbb_amd:0.14
+sudo docker run -v "$(pwd)/modules/:/home/ubuntu/work/modules/" -v "$(pwd)/$param1:/home/ubuntu/work/todo.sh" -it --rm -u ubuntu bbb_amd:0.14
